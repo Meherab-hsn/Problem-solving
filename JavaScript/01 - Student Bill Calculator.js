0ea4ace3f4dd0981d995b8d6ea710a5878bcae7d
meherab hossain <meherabhosssain@Meherab-Hossain.local>
2026-09-18 14:54:35 +0600

    1st problem

---
+++ JavaScript/01 - Student Bill Calculator.js
@@ -0,0 +1,15 @@
+/*  
+Problem: Student Bill Calculator
+
+Create a JavaScript program that:
+
+1. Takes the prices of 3 products.
+2. Calculates the **total price**.
+3. Calculates the **average price**.
+4. Applies a **10% discount** and calculates the discount amount.
+5. Calculates the **final price after discount**.
+6. Divides the final price between **2 people**.
+7. Converts **125 minutes** into hours and remaining minutes.
+8. Swaps the prices of Product 1 and Product 2 and displays the new values.
+*/ 
+
